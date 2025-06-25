6609f4b571affbffa4c7fe7c238de88c3e68fc4d
Atharva Bhargava <bhargavaatharva.10.com>
2025-06-26 01:50:00 +0530

    fix: replaced undefined API_APPID with VITE env variables

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ const getcal=async()=>{
   const response= await fetch("https://trackapi.nutritionix.com/v2/natural/nutrients",{
     method: "POST",
     headers:{
-      "x-app-id": API_APPID,
-      "x-app-key": API_KEY,
+      "x-app-id": appId,
+      "x-app-key": appKey,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({query})
